6fd894bb37679404d5db1297599074d5f1a7f572
Josh Bergstrom <jbergs12@byu.edu>
2024-11-06 10:08:59 -0700

    knn nb rf and svm

--- ggg_recipe.R
+++ ggg_recipe.R
@@ -1,7 +1,7 @@
 ggg_recipe <- function(traindata){
-  recipe(ACTION~., data=traindata) |> 
-    step_mutate_at(all_numeric_predictors(), fn = factor) |>
-    step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) |>
+  recipe(type~., data=traindata) |> 
+    step_mutate_at(all_nominal_predictors(), fn = factor) |>
+    step_lencode_glm(all_nominal_predictors(), outcome = vars(type)) |>
     #step_zv(all_predictors()) |> 
     step_normalize(all_numeric_predictors()) #|>
   #step_pca(all_predictors(), threshold = .85)
@@ -27,9 +27,9 @@ run_cv <- function(wf, folds, grid, metric=metric_set(rmse), cores=8, parallel =
 
 ggg_smote_recipe <- function(traindata, neighbors){
   library(themis)
-  recipe(ACTION~., data=traindata) |> 
-    step_mutate_at(all_numeric_predictors(), fn = factor) |>
-    step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) |>
+  recipe(type~., data=traindata) |> 
+    step_mutate_at(all_nominal_predictors(), fn = factor) |>
+    step_lencode_mixed(all_nominal_predictors(), outcome = vars(type)) |>
     step_normalize(all_numeric_predictors()) |>
     step_pca(all_predictors(), threshold = .85) |> 
     step_smote(all_outcomes(), neighbors=neighbors)
